7a378ef46bde566582313227025bffb61d5eb64c
tgilkrog <33146669+tgilkrog@users.noreply.github.com>
2025-06-16 21:47:41 +0200

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,51 @@
+﻿var house_svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>`;
+var admin_svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M78.6 5C69.1-2.4 55.6-1.5 47 7L7 47c-8.5 8.5-9.4 22-2.1 31.6l80 104c4.5 5.9 11.6 9.4 19 9.4l54.1 0 109 109c-14.7 29-10 65.4 14.3 89.6l112 112c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-112-112c-24.2-24.2-60.6-29-89.6-14.3l-109-109 0-54.1c0-7.5-3.5-14.5-9.4-19L78.6 5zM19.9 396.1C7.2 408.8 0 426.1 0 444.1C0 481.6 30.4 512 67.9 512c18 0 35.3-7.2 48-19.9L233.7 374.3c-7.8-20.9-9-43.6-3.6-65.1l-61.7-61.7L19.9 396.1zM512 144c0-10.5-1.1-20.7-3.2-30.5c-2.4-11.2-16.1-14.1-24.2-6l-63.9 63.9c-3 3-7.1 4.7-11.3 4.7L352 176c-8.8 0-16-7.2-16-16l0-57.4c0-4.2 1.7-8.3 4.7-11.3l63.9-63.9c8.1-8.1 5.2-21.8-6-24.2C388.7 1.1 378.5 0 368 0C288.5 0 224 64.5 224 144l0 .8 85.3 85.3c36-9.1 75.8 .5 104 28.7L429 274.5c49-23 83-72.8 83-130.5zM56 432a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>';
+
+const environments = [
+  {
+    label: 'Local',
+    domain: (val) => `http://${val.trim()}.localhost/`
+  },
+  {
+    label: 'Main',
+    domain: (val) => {
+      const v = val.trim();
+      return /^https?:\/\//.test(v) ? v : `https://${v}.dk`;
+    }
+  },
+  {
+    label: 'Staging',
+    domain: (val) => {
+      const v = val.trim();
+      return /^https?:\/\//.test(v) ? v : `https://${v}-staging.wexohosting.com`;
+    }
+  },
+  {
+    label: 'Test',
+    domain: (val) => {
+      const v = val.trim();
+      return /^https?:\/\//.test(v) ? v : `https://${v}-test.wexohosting.com`;
+    }
+  }
+];
+
+window.buttonGroups = environments.map(({ label, domain }) => ({
+  label,
+  buttons: [
+    {
+      icon: house_svg,
+      title: `${label} Site`,
+      name: 'main',
+      buildUrl: domain
+    },
+    {
+      icon: admin_svg,
+      title: 'Admin',
+      name: 'admin',
+      buildUrl: (val) => {
+        const url = domain(val).replace(/\/+$/, '');
+        return `${url}/admin`;
+      }
+    }
+  ]
+}));
